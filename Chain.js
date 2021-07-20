8c77c8665edcb683ed9e94a432db56736daae772
Aarav Jalan <83217965+Gyrogod0@users.noreply.github.com>
2021-07-20 14:50:52 +0530

    Add files via upload

---
+++ Chain.js
@@ -0,0 +1,17 @@
+class Chain{
+    constructor(bodyA, bodyB){
+        var options = {
+            bodyA : bodyA,
+            bodyB : bodyB,
+            stiffness : 0.04,
+            length : 10
+        }
+        this.chain = Constraint.create(options);
+
+        World.add(world, this.chain);
+    }
+    display(){
+        strokeWeight(3);
+        line(bird.body.position.x, bird.body.position.y, cLog.body.position.x, cLog.body.position.y);
+    }
+}
